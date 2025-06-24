d86393c3bc45696e94aa5e6cd242af055222467e
samet <saltuntas717@gmail.com>
2025-06-24 15:39:00 +0300

    Production and Consumption current values are basically ready

--- SyntheticValueGenerator.py
+++ SyntheticValueGenerator.py
@@ -16,16 +16,16 @@ class SyntheticValueGenerator:
         current_date_time = datetime.datetime.now()
         date_converted = current_date_time.strftime("%d/%m/%Y")
         time_converted = current_date_time.strftime("%H:%M:%S")
-        production_value = self._generate_random_value()
-        consumption_value = self._generate_random_value()
-        balance = production_value - consumption_value
-        print(f"Production: {production_value}")
-        print(f"Consumption: {consumption_value}")
+        self.production_value = self._generate_random_value()
+        self.consumption_value = self._generate_random_value()
+        balance = self.production_value - self.consumption_value
+        print(f"Production: {self.production_value}")
+        print(f"Consumption: {self.consumption_value}")
         print(f"Balance: {balance}")
         print(f"Date: {date_converted}")
         print(f"Time: {time_converted}\n")
 
-        self.db_manager.insert_energy_values(date_converted, time_converted, production_value, consumption_value, balance)
+        self.db_manager.insert_energy_values(date_converted, time_converted, self.production_value, self.consumption_value, balance)
 
         self._timer = threading.Timer(10.0, self._run_periodic)
         self._timer.start()
--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, jsonify
 from SyntheticValueGenerator import SyntheticValueGenerator
 from DatabaseManager import DatabaseManager
 
@@ -17,9 +16,19 @@ class FlaskApp:
 
     def _register_routes(self):
         self.app.route("/")(self.main_page)
+        self.app.route("/api/data")(self.current_data)
 
     def main_page(self):
         return render_template("index.html")
+
+    def current_data(self):
+        production_value = self.synthetic_value_generator.production_value
+        consumption_value = self.synthetic_value_generator.consumption_value
+
+        return jsonify({
+            "production_value": production_value,
+            "consumption_value": consumption_value
+        })
 
     def run(self, **kwargs):
         self.app.run(**kwargs)
